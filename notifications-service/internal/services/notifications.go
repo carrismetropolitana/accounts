7e52e19c64dc1c57b6e9322b89d57fe4b24319a7
jusimen <jusi.monteiro@protonmail.com>
2025-08-20 15:23:33 +0100

    fix: Update notification message to use stop name instead of stop ID

--- notifications-service/internal/services/notifications.go
+++ notifications-service/internal/services/notifications.go
@@ -81,7 +81,7 @@ func notificationService(RedisService *RedisService, firebaseService *FirebaseSe
 
 					// Send notification to firebase
 					title := "OLHÓ Autocarro 👀 🚌 "
-					body := fmt.Sprintf("O autocarro %s está a chegar à paragem %s", vehicle.LineId, notification.StopId)
+					body := fmt.Sprintf("O autocarro %s está a chegar à paragem %s", vehicle.LineId, notification.StopName)
 					err := firebaseService.SendToTopic(notification.Id, title, body)
 					if err != nil {
 						fmt.Printf("Error sending notification for vehicle %s and stop %s: %v\n", vehicle.Id, notification.StopId, err)
