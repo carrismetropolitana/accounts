e5b750906aca7ae1e343a12be117b95b7ca0e284
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-17 12:10:34 +0100

    Added types package

---
+++ packages/types/src/account.ts
@@ -0,0 +1,32 @@
+/* * */
+
+import { DeviceSchema } from '@/device.js';
+import { FavoritesSchema } from '@/favorites.js';
+import { NotificationsSchema } from '@/notifications.js';
+import { PersonaSchema } from '@/persona.js';
+import { ProfileSchema } from '@/profile.js';
+import { WidgetSchema } from '@/widgets.js';
+import { DocumentSchema } from '@tmlmobilidade/types';
+import { z } from 'zod';
+
+/* * */
+
+export const ACCOUNT_ROLE_VALUES = ['owner', 'admin', 'user'] as const;
+
+export const AccountRoleSchema = z.enum(ACCOUNT_ROLE_VALUES);
+
+export type AccountRole = z.infer<typeof AccountRoleSchema>;
+
+/* * */
+
+export const AccountSchema = DocumentSchema.extend({
+	devices: z.array(DeviceSchema).default([]),
+	favorites: FavoritesSchema,
+	notifications: NotificationsSchema,
+	persona: PersonaSchema,
+	profile: ProfileSchema,
+	role: AccountRoleSchema.default('user'),
+	widgets: z.array(WidgetSchema).default([]),
+}).strict();
+
+export type Account = z.infer<typeof AccountSchema>;
