f9a19e54f26edbff216f17ce054de6d89006c94d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-18 00:50:36 +0100

    Remove url definition for persona image id

--- packages/types/src/persona.ts
+++ packages/types/src/persona.ts
@@ -7,7 +7,7 @@ import { z } from 'zod';
 export const PersonaSchema = z.object({
 	accent_color: z.string().regex(/^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/).nullable().default(null),
 	image_history: z.array(z.string()).default([]),
-	image_id: z.string().url().nullable().default(null),
+	image_id: z.string().nullable().default(null),
 });
 
 export type Persona = z.infer<typeof PersonaSchema>;
