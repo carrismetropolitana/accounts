d44a03b799cf7c4ba9a3082aef2d86c26c523556
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-24 18:07:59 +0100

    Add log

--- apps/server/src/endpoints/accounts/accounts.controller.ts
+++ apps/server/src/endpoints/accounts/accounts.controller.ts
@@ -65,6 +65,7 @@ export class AccountsController {
 	 * @param reply Fastify reply
 	 */
 	static async update(request: FastifyRequest<{ Body: Account }>, reply: FastifyReply<Account>) {
+		console.log('UPDATE REQUEST', request.device_id);
 		// Find the account by Account ID. If not found, throw 404.
 		const foundAccount = await accounts.findByDeviceId(request.device_id);
 		if (!foundAccount) throw new HttpException(HttpStatus.NOT_FOUND, 'Account not found');
