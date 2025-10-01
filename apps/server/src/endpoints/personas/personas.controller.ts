4dbe6e40c8ebd3c11b6d95bbf396ab33c8831f97
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-10-01 22:32:43 +0100

    Redirect to storage bucket

--- apps/server/src/endpoints/personas/personas.controller.ts
+++ apps/server/src/endpoints/personas/personas.controller.ts
@@ -3,7 +3,6 @@
 import { getRandomPersonaImageId } from '@/services/personas.js';
 import { type FastifyReply, type FastifyRequest } from '@tmlmobilidade/connectors';
 import { HttpStatus } from '@tmlmobilidade/lib';
-import fs from 'node:fs';
 
 /* * */
 
@@ -16,10 +15,10 @@ export class PersonasController {
 	 * @param reply Fastify reply
 	 */
 	static async getImageById(request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply<ReadableStream>) {
-		// Read the image file from the filesystem
-		const fileStream = fs.createReadStream(`/app/dist/public/personas/${request.params.id}.png`);
-		// Send the image file as a response
-		return reply.type('image/png').send(fileStream);
+		// Persona images are stored in the storage bucket
+		const redirectUrl = `https://storage.carrismetropolitana.pt/static/app/assets/personas/${request.params.id}.png`;
+		// Redirect to the image URL using a 307 Temporary Redirect status code
+		return reply.redirect(redirectUrl, HttpStatus.TEMPORARY_REDIRECT);
 	}
 
 	/**
