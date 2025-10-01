b6dba58566dc22f2f857d35528d6e74d6d9c322e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-10-01 15:24:22 +0100

    Re-enable default persona

--- apps/server/src/endpoints/accounts/accounts.controller.ts
+++ apps/server/src/endpoints/accounts/accounts.controller.ts
@@ -31,8 +31,7 @@ export class AccountsController {
 			.parse({
 				created_at: Dates.now('Europe/Lisbon').unix_timestamp,
 				devices: [{ device_id: randomDeviceId }],
-				// persona: { image_id: getRandomPersonaImageId() },
-				persona: { image_id: '1' },
+				persona: { image_id: getRandomPersonaImageId() },
 				updated_at: Dates.now('Europe/Lisbon').unix_timestamp,
 			});
 		// Save the new account to the database
