20c22f47afad757e65e9b5ac98c2ad4d86c20c90
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-10-01 23:01:57 +0100

    Add local mapping of persona image IDs

--- apps/server/src/services/personas.ts
+++ apps/server/src/services/personas.ts
@@ -1,16 +1,12 @@
 /* * */
 
-import fs from 'node:fs';
+import { availablePersonaImageIds } from './persona_ids.js';
 
 /**
  * Retrieves a random persona image ID from the available images.
  * @returns A random persona image ID.
  */
 export function getRandomPersonaImageId(): string {
-	const imagesDir = '/app/dist/public/personas';
-	const availableImages = fs.readdirSync(imagesDir);
-	const randomIndex = Math.floor(Math.random() * availableImages.length);
-	const randomSelection = availableImages[randomIndex];
-	const randomSelectionId = randomSelection.replace('.png', '');
-	return randomSelectionId;
+	const randomIndex = Math.floor(Math.random() * availablePersonaImageIds.length);
+	return availablePersonaImageIds[randomIndex];
 }
